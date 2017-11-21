80103ff816713817230b97cd0ad73973b6be00e7
komota <komotatit@gmail.com>
2017-11-22 04:11:59 +0900

    [feature - AWSgan.py] 本物見たら G の更新2回やってたのでパクる

--- models/AWSgan.py
+++ models/AWSgan.py
@@ -195,6 +195,9 @@ def train():
             # generatorを更新
             g_loss = dcgan.train_on_batch(n_learn, [1]*BATCH_SIZE)
            
+            # generatorを再度更新
+            g_loss = dcgan.train_on_batch(n_learn, [1]*BATCH_SIZE)
+            
             # 学習が済んだ段階で G から画像を再生成
             g_images = generator.predict(n_learn, verbose=0)
             
