51ca1005d28f7e538d81ff5d8fa72c603df15ed3
komota <komotatit@gmail.com>
2017-11-23 22:17:55 +0900

    [feature - main.py] モデルとノイズのデータを削除するか，設定ファイルで設定できるように変更

--- main.py
+++ main.py
@@ -2,6 +2,21 @@
 
 # from models import gan
 from models import AWSgan as gan
+from setting import IS_RESET_MODEL
+from setting import IS_RESET_NOIZE
+
+import os
 
 if __name__ == "__main__":
+    # IS_RESET_MODEL が True なら， save_model
+    # IS_RESET_NOIZE が True なら， save_noize
+    # を削除する
+    if IS_RESET_MODEL == True:
+        os.remove("tmp/save_models/discriminator.json")
+        os.remove("tmp/save_models/discriminator.h5")
+        os.remove("tmp/save_models/generator.json")
+        os.remove("tmp/save_models/generator.h5")
+    if IS_RESET_NOIZE == True:
+        os.remove("tmp/save_noizes/*.dill")
+
     gan.train()
