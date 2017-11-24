b1ca29279d5041460d9000ed8764564947b1234e
komota <komotatit@gmail.com>
2017-11-24 16:53:56 +0900

    [fix - main.py] IS_RESET が True でも，ファイルがなければ削除しないように変更

--- main.py
+++ main.py
@@ -6,20 +6,19 @@ from setting import IS_RESET_MODEL
 from setting import IS_RESET_NOIZE
 
 import os
+import glob
 
 if __name__ == "__main__":
     # IS_RESET_MODEL が True なら， save_model
     # IS_RESET_NOIZE が True なら， save_noize
     # を削除する
     if IS_RESET_MODEL == True:
-        os.remove("tmp/save_models/discriminator.json")
-        os.remove("tmp/save_models/discriminator.h5")
-        os.remove("tmp/save_models/generator.json")
-        os.remove("tmp/save_models/generator.h5")
+        files = glob.glob("tmp/save_models/*.*")
+        for f in files:
+            os.remove(f)
     if IS_RESET_NOIZE == True:
-        os.remove("tmp/save_noizes/forLearn_0.dill")
-        os.remove("tmp/save_noizes/forLearn_1.dill")
-        os.remove("tmp/save_noizes/forLearn_2.dill")
-        os.remove("tmp/save_noizes/forLearn_3.dill")
+        files = glob.glob("tmp/save_noizes/*.*")
+        for f in files:
+            os.remove(f)
 
     gan.train()
