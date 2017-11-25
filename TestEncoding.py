f9fbbddcb5dc5bb9053154bd952475af4173acba
Komota <komotatit@gmail.com>
2017-11-25 19:47:20 +0900

    [fix - TestEncoder.py] 調整

--- TestEncoding.py
+++ TestEncoding.py
@@ -7,6 +7,9 @@ from keras.models import Sequential
 from keras.models import model_from_json
 from keras.optimizers import Adam
 
+from setting import G_LR
+from setting import G_BETA
+
 model_path = "tmp/save_models/"
 epoch      = 100
 
@@ -23,20 +26,20 @@ with open(model_path+"encoder.json", "r", encoding="utf-8") as f:
 model = Sequential([encoder, generator])
 model.trainable = False
 g_opt = Adam(lr=G_LR, beta_1=G_BETA)
-model.compile(loss="mean_squired_error", optimizer=g_opt)
+model.compile(loss="mean_squared_error", optimizer=g_opt)
 
 
 imgpaths = glob.glob("tmp/friends/*.png")
 
 for imgpath in imgpaths:
     real = cv2.imread(imgpath)
-    cv2.imshow("real", real)
     real = real.astype(np.float32)
     real = (real-127.5)/127.5
-    fake = model.predict(real, verbose=0)
+    fake = model.predict(np.array([real]), verbose=0)[0]
     fake = fake*127.5 + 127.5
     fake = fake.astype(np.uint8)
-    cv2.imshow("fake", fake)
+    cv2.imshow("real", cv2.resize(real, (160, 160), interpolation=cv2.INTER_LINEAR))
+    cv2.imshow("fake", cv2.resize(fake, (160, 160), interpolation=cv2.INTER_LINEAR))
     key = cv2.waitKey(0)
     if key == ord("a"):
         break
