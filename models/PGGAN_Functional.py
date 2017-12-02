5a7bbde00bf64e573e824079fdf1d15960409cca
komota <komotatit@gmail.com>
2017-12-03 03:40:02 +0900

    [fix - PGGAN_Functional] フェードレイヤーを Dense で作る試み

--- models/PGGAN_Functional.py
+++ models/PGGAN_Functional.py
@@ -240,12 +240,12 @@ def train():
             # running Fade-in
             # alpha を調節しながら学習する為，エポックごとにコンパイルする
             # フェードイン用のレイヤーを用意
-            fade_D1 = Dense((4*2**i, 4*2**i, 3), trainable=False)
-            fade_D2 = Dense((4*2**i, 4*2**i, 3), trainable=False)
-            fade_G1 = Dense((4*2**i, 4*2**i, 3), trainable=False)
-            fade_G2 = Dense((4*2**i, 4*2**i, 3), trainable=False)
-            fade_G3 = Dense((4*2**i, 4*2**i, 3), trainable=False)
-            fade_G4 = Dense((4*2**i, 4*2**i, 3), trainable=False)
+            fade_D1 = Conv2D(3, (1, 1), trainable=False)
+            fade_D2 = Conv2D(3, (1, 1), trainable=False)
+            fade_G1 = Conv2D(3, (1, 1), trainable=False)
+            fade_G2 = Conv2D(3, (1, 1), trainable=False)
+            fade_G3 = Conv2D(3, (1, 1), trainable=False)
+            fade_G4 = Conv2D(3, (1, 1), trainable=False)
             # 学習モデルを構築
             input_D   = Input((4*2**i, 4*2**i, 3))
             output_D1 = AveragePooling2D((2, 2))(input_D)
